58b09185289070acd565abff2b2bf194e3f88114
Andreas Ablahad <andreas.ablahad@elev.ga.ntig.se>
2022-09-06 10:26:59 +0200

    Första commit

---
+++ lucktext/Program.cs
@@ -0,0 +1,23 @@
+﻿string name = "Andreas";
+
+
+
+Console.WriteLine("Hej vad heter du?");
+name = Console.ReadLine();
+
+
+
+Console.WriteLine($"Hej {name}!");
+
+Console.ReadLine();
+
+Console.WriteLine($"Hur mår du idag {name}? ");
+name = Console.ReadLine();
+
+
+
+Console.WriteLine("Vad gillar du att göra?");
+
+string Sport = Console.ReadLine();
+
+

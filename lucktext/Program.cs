51f1153bda911065a95bf82f9ea3a9ebce4873e2
Andreas Ablahad <andreas.ablahad@elev.ga.ntig.se>
2022-09-06 10:41:13 +0200

    hehe blev precis klar

--- lucktext/Program.cs
+++ lucktext/Program.cs
@@ -5,21 +5,31 @@
 Console.WriteLine("Hej vad heter du?");
 name = Console.ReadLine();
 
+Console.WriteLine($"Hur mår du idag {name}? ");
+string health = Console.ReadLine();
 
-
-Console.WriteLine($"Hej {name}!");
+Console.WriteLine($"Hej {name}! Jag mår också {health}");
 
 Console.ReadLine();
 
-Console.WriteLine($"Hur mår du idag {name}? ");
-name = Console.ReadLine();
-
-
-
-
-
 Console.WriteLine("Vad gillar du att göra?");
+string sport = Console.ReadLine();
 
-string Sport = Console.ReadLine();
+
+
+Console.WriteLine("Okej vad kul och träffas ha en trevlig dag Hejdå!");
+Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
 
 
